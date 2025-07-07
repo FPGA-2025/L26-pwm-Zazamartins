2df74b3eaf0bc7fd0203399cfb2b4ee1a0cd1ace
Zazamartins <isaacmrtnsss@gmail.com>
2025-07-07 15:10:26 -0300

    Ok

--- pwm.v
+++ pwm.v
@@ -6,4 +6,24 @@ module PWM (
     output reg pwm_out
 );
 
+reg [15:0] counter;
+
+always @(posedge clk or negedge rst_n) begin
+    if(!rst_n) begin
+        counter <= 16'b0;
+        pwm_out <= 1'b0;
+    end else begin
+        if(counter < duty_cycle) begin
+            pwm_out <= 1'b1;
+        end else begin
+            pwm_out <= 1'b0;
+        end
+        if(counter == period - 1) begin
+            counter <= 0;
+        end else begin
+            counter <= counter + 1;
+        end
+    end
+end
+
 endmodule
